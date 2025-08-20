2ada85340a52591b0570ccb2daac78f1c6af6c57
Asghar Ali <asghar.usman0017@gmail.com>
2025-08-20 12:12:41 -0700

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://anjum-marketing.inplsoftwares.online",
+        target: "https://anjum-chemical.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
