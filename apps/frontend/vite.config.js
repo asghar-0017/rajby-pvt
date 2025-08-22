5724c0effdff5e640bbd1af8cb07b3fdb4caee0f
Asghar Ali <asghar.usman0017@gmail.com>
2025-08-22 03:41:58 -0700

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://anjum-parts.inplsoftwares.online",
+        target: "https://adnan-power.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
