957fceb9bb00401337dbbd338ab864b65e0c8124
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-23 08:17:31 +0500

    .

--- apps/frontend/src/API/Api.js
+++ apps/frontend/src/API/Api.js
@@ -46,7 +46,7 @@ const API_CONFIG = {
 
 const api = axios.create({
   // baseURL: "https://fbrtestcase.inplsoftwares.online/api",
-  baseURL: "https://central-timber.inplsoftwares.online/api",
+  baseURL: "https://signs-now.inplsoftwares.online/api",
   // You can add headers or other config here if needed
 });
 
--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://central-timber.inplsoftwares.online",
+        target: "https://signs-now.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
