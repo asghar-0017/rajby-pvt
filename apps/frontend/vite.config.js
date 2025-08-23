e98be413fd2e5c94320a56c1f18f2945a09b2aa5
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-23 08:12:26 +0500

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://anwer-tex.inplsoftwares.online",
+        target: "https://central-timber.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
