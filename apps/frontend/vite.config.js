f557c39cf264de24f7fd2691ba6ef7352fe24157
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-23 08:47:43 +0500

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://paramount-corp.inplsoftwares.online",
+        target: "https://tulip-towel.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
