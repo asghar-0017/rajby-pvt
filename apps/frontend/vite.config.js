d1b9ae2af0a99f0b07aff90dba6d4967457ecb7e
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-23 07:58:29 +0500

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://syahimpex.inplsoftwares.online",
+        target: "https://anjum-chemical.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
