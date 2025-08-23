c49381e7e1af5eaeaf94df22a6a51a8981a3bdc8
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-23 09:03:25 +0500

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://united-tubes.inplsoftwares.online",
+        target: "https://adnan-power.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
