bf3180c777e54a9563c59ec62e5f6e6d8be88502
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-23 08:39:20 +0500

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -44,7 +44,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://pak-progressive.inplsoftwares.online",
+          "https://rainbow-textile.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -58,8 +58,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://pak-progressive.inplsoftwares.online",
-      "https://pak-progressive.inplsoftwares.online",
+      "https://rainbow-textile.inplsoftwares.online",
+      "https://rainbow-textile.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://pak-progressive.inplsoftwares.online",
+        target: "https://rainbow-textile.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
