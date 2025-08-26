4a9f9a75a46d7789eb1892e33ace5f3ef5ea5e38
Asghar Ali <asghar.usman0017@gmail.com>
2025-08-25 23:34:57 -0700

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://signs-now.inplsoftwares.online",
+        target: "https://central-timber.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
