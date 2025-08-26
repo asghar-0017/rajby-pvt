a4f57ab45e26377360fe5c9b5c3a82742b9a8989
Asghar Ali <asghar.usman0017@gmail.com>
2025-08-25 23:25:40 -0700

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -44,7 +44,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://mayanipoly.inplsoftwares.online",
+          "https://hubpoly-packages.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -58,8 +58,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://mayanipoly.inplsoftwares.online",
-      "https://mayanipoly.inplsoftwares.online",
+      "https://hubpoly-packages.inplsoftwares.online",
+      "https://hubpoly-packages.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
--- apps/frontend/src/component/BuyerModal.jsx
+++ apps/frontend/src/component/BuyerModal.jsx
@@ -215,7 +215,7 @@ const BuyerModal = ({ isOpen, onClose, onSave, buyer }) => {
       setBuyerRegistrationHint("");
 
       const response = await fetch(
-        "https://mayanipoly.inplsoftwares.online/api/buyer-check",
+        "https://hubpoly-packages.inplsoftwares.online/api/buyer-check",
         {
           method: "POST",
           headers: {
