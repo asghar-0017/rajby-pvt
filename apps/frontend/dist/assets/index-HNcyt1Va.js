3d6336837c7831ec765a81a66a17c977451c4590
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-30 04:46:57 +0500

    .

--- apps/frontend/dist/assets/index-HNcyt1Va.js
+++ apps/frontend/dist/assets/index-HNcyt1Va.js
@@ -46412,7 +46412,7 @@ const wE = (e) => {
   },
   Cn = {
     apiKey: "https://gw.fbr.gov.pk",
-    apiKeyLocal: "https://pakleather.inplsoftwares.online/api",
+    apiKeyLocal: "https://tulip-towel.inplsoftwares.online/api",
     get sandBoxTestToken() {
       const e = Ad.getSandboxToken();
       return (
@@ -46447,7 +46447,7 @@ const wE = (e) => {
     },
   },
   Qt = Xr.create({
-    baseURL: "https://pakleather.inplsoftwares.online/api",
+    baseURL: "https://tulip-towel.inplsoftwares.online/api",
   });
 Qt.interceptors.request.use(
   (e) => {
@@ -52407,7 +52407,7 @@ const x0 = new nve(),
           try {
             (I(!0), R(""));
             const U = await fetch(
-              "https://pakleather.inplsoftwares.online/api/buyer-check",
+              "https://tulip-towel.inplsoftwares.online/api/buyer-check",
               {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -79883,7 +79883,7 @@ const YS = (e) => {
           try {
             const ye = await (
               await fetch(
-                "https://pakleather.inplsoftwares.online/api/buyer-check",
+                "https://tulip-towel.inplsoftwares.online/api/buyer-check",
                 {
                   method: "POST",
                   headers: { "Content-Type": "application/json" },
--- apps/frontend/src/component/BuyerModal.jsx
+++ apps/frontend/src/component/BuyerModal.jsx
@@ -215,7 +215,7 @@ const BuyerModal = ({ isOpen, onClose, onSave, buyer }) => {
       setBuyerRegistrationHint("");
 
       const response = await fetch(
-        "https://pakleather.inplsoftwares.online/api/buyer-check",
+        "https://tulip-towel.inplsoftwares.online/api/buyer-check",
         {
           method: "POST",
           headers: {
