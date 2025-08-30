fff273bddc20ed37e25bc445ceee5d0951ee603e
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-30 05:10:12 +0500

    .

--- apps/frontend/dist/assets/index-HNcyt1Va.js
+++ apps/frontend/dist/assets/index-HNcyt1Va.js
@@ -46412,7 +46412,7 @@ const wE = (e) => {
   },
   Cn = {
     apiKey: "https://gw.fbr.gov.pk",
-    apiKeyLocal: "https://rainbow-textile.inplsoftwares.online/api",
+    apiKeyLocal: "https://spoltlit.inplsoftwares.online/api",
     get sandBoxTestToken() {
       const e = Ad.getSandboxToken();
       return (
@@ -46447,7 +46447,7 @@ const wE = (e) => {
     },
   },
   Qt = Xr.create({
-    baseURL: "https://rainbow-textile.inplsoftwares.online/api",
+    baseURL: "https://spoltlit.inplsoftwares.online/api",
   });
 Qt.interceptors.request.use(
   (e) => {
@@ -52407,7 +52407,7 @@ const x0 = new nve(),
           try {
             (I(!0), R(""));
             const U = await fetch(
-              "https://rainbow-textile.inplsoftwares.online/api/buyer-check",
+              "https://spoltlit.inplsoftwares.online/api/buyer-check",
               {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
@@ -79883,7 +79883,7 @@ const YS = (e) => {
           try {
             const ye = await (
               await fetch(
-                "https://rainbow-textile.inplsoftwares.online/api/buyer-check",
+                "https://spoltlit.inplsoftwares.online/api/buyer-check",
                 {
                   method: "POST",
                   headers: { "Content-Type": "application/json" },
--- apps/frontend/src/component/InvoiceUploader.jsx
+++ apps/frontend/src/component/InvoiceUploader.jsx
@@ -724,7 +724,7 @@ const InvoiceUploader = ({ onUpload, onClose, isOpen, selectedTenant }) => {
       }
       try {
         const resp = await fetch(
-          "https://rainbow-textile.inplsoftwares.online/api/buyer-check",
+          "https://spoltlit.inplsoftwares.online/api/buyer-check",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
