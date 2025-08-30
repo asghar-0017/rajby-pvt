2863b77a41799c778691ac864cb1eeec04455de6
Wahaj Ismail <wahajismail66@gmail.com>
2025-08-30 05:54:04 +0500

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://biomedics.inplsoftwares.online",
+        target: "https://aferoz.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
