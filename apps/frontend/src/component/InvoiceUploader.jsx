dfd71edd93d876ff41818b4d4a13bc297910f8e4
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-01 16:11:30 -0700

    .

--- apps/frontend/src/component/InvoiceUploader.jsx
+++ apps/frontend/src/component/InvoiceUploader.jsx
@@ -646,7 +646,7 @@ const InvoiceUploader = ({ onUpload, onClose, isOpen, selectedTenant }) => {
       }
       try {
         const resp = await fetch(
-          "https://einv-aaafm.inplsoftwares.online/api/buyer-check",
+          "https://premierpipe.inplsoftwares.online/api/buyer-check",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
