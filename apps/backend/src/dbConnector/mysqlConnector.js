57cc57f5f9c67fd3e323a9cb953db04b3d7e717f
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-01 06:21:33 -0700

    created another user

--- apps/backend/src/dbConnector/mysqlConnector.js
+++ apps/backend/src/dbConnector/mysqlConnector.js
@@ -34,15 +34,15 @@ const mysqlConnector = async (dbConfig, logger) => {
 const initializeAdminUser = async () => {
   try {
     const adminExists = await AdminUser.findOne({
-      where: { email: "operations-01@excel-pk.com" },
+      where: { email: "Sales-01@excel-pk.com" },
     });
 
     if (!adminExists) {
       const bcrypt = await import("bcryptjs");
-      const hashedPassword = await bcrypt.hash("r_operations-01pasJK76^h", 10);
+      const hashedPassword = await bcrypt.hash("r_Sales-01pasJK76^h", 10);
 
       await AdminUser.create({  
-        email: "operations-01@excel-pk.com",
+        email: "Sales-01@excel-pk.com",
         password: hashedPassword,
         is_verify: true,
         role: "admin",
