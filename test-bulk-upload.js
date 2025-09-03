2aac1af30c86b7cb091a2cc918592766e857154b
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-03 07:58:24 +0500

    .

--- test-bulk-upload.js
+++ test-bulk-upload.js
@@ -49,7 +49,7 @@ const testBulkUpload = async () => {
       sampleInvoice: testInvoices[0]
     });
 
-    const response = await fetch('https://maritimeseafood.inplsoftwares.online/api/tenant/tenant_1756409312403_uo1hmt4tz/invoices/bulk', {
+    const response = await fetch('https://habibsugar.inplsoftwares.online/api/tenant/tenant_1756409312403_uo1hmt4tz/invoices/bulk', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
