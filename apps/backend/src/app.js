0dc2c351d131cc14dcfec2df9eaad885d6ac4784
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-04 06:53:02 +0500

    new build

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://spoltlit.inplsoftwares.online",
+          "https://rainbow-textile.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://spoltlit.inplsoftwares.online",
-      "https://spoltlit.inplsoftwares.online",
+      "https://rainbow-textile.inplsoftwares.online",
+      "https://rainbow-textile.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
--- apps/frontend/src/component/BuyerModal.jsx
+++ apps/frontend/src/component/BuyerModal.jsx
@@ -322,7 +322,7 @@ const BuyerModal = ({ isOpen, onClose, onSave, buyer }) => {
       setBuyerRegistrationHint("");
 
       const response = await fetch(
-        "https://spoltlit.inplsoftwares.online/api/buyer-check",
+        "https://rainbow-textile.inplsoftwares.online/api/buyer-check",
         {
           method: "POST",
           headers: {
