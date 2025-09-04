60e2de47a30f9a650dcc18cfa20ae80f7f108c56
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-04 07:04:01 +0500

    new build

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://tulip-towel.inplsoftwares.online",
+        target: "https://pakleather.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
