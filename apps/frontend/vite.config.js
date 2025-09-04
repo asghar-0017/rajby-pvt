fdc62fb4fdb4cdb3ef93502022540dac106432f1
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-04 07:09:06 +0500

    new build

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://einv-aaafm.inplsoftwares.online",
+        target: "https://premierpipe.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
