b4f95346e316584899eaf245dcab931544cfc6bf
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-04 06:00:21 +0500

    new build

--- apps/frontend/src/component/BuyerModal.jsx
+++ apps/frontend/src/component/BuyerModal.jsx
@@ -322,7 +322,7 @@ const BuyerModal = ({ isOpen, onClose, onSave, buyer }) => {
       setBuyerRegistrationHint("");
 
       const response = await fetch(
-        "https://adnan-textile.inplsoftwares.online/api/buyer-check",
+        "https://adnan-power.inplsoftwares.online/api/buyer-check",
         {
           method: "POST",
           headers: {
