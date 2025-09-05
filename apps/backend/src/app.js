dc345ef4810e330633c20e25b0da562575ddd28c
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-05 07:58:57 +0500

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://biomedics.inplsoftwares.online",
+          "https://aqmsburhani.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://biomedics.inplsoftwares.online",
-      "https://biomedics.inplsoftwares.online",
+      "https://aqmsburhani.inplsoftwares.online",
+      "https://aqmsburhani.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
