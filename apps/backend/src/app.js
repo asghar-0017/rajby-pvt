95bc81f1187af77c1a16a17e30a06c3fb7015d5a
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-05 08:17:07 +0500

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://machinery-zone.inplsoftwares.online",
+          "https://signs-now.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://machinery-zone.inplsoftwares.online",
-      "https://machinery-zone.inplsoftwares.online",
+      "https://signs-now.inplsoftwares.online",
+      "https://signs-now.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
