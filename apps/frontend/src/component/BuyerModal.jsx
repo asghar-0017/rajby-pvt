1ebcc3a022c690edba09a951e31f41cff2355aeb
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-07 15:30:50 -0700

    .

--- apps/frontend/src/component/BuyerModal.jsx
+++ apps/frontend/src/component/BuyerModal.jsx
@@ -331,7 +331,7 @@ const BuyerModal = ({ isOpen, onClose, onSave, buyer }) => {
       setBuyerRegistrationHint("");
 
       const response = await fetch(
-        "https://getzpharma.inplsoftwares.online/api/buyer-check",
+        "https://pakburma.inplsoftwares.online/api/buyer-check",
         {
           method: "POST",
           headers: {
