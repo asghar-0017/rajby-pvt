f9b26220ced2bfeb1f7abe83a0e9b937c4d75d6d
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-09 12:50:20 -0700

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://superstar.inplsoftwares.online",
+        target: "https://maritimefisheries.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
