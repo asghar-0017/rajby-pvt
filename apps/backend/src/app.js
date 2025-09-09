bbe48e51c708714ac19a468b31b4ef6ccd08c9c3
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-09 12:56:20 -0700

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://bktextile.inplsoftwares.online",
+          "https://meditec.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://bktextile.inplsoftwares.online",
-      "https://bktextile.inplsoftwares.online",
+      "https://meditec.inplsoftwares.online",
+      "https://meditec.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
--- apps/backend/src/dbConnector/mysqlConnector.js
+++ apps/backend/src/dbConnector/mysqlConnector.js
@@ -34,15 +34,15 @@ const mysqlConnector = async (dbConfig, logger) => {
 const initializeAdminUser = async () => {
   try {
     const adminExists = await AdminUser.findOne({
-      where: { email: "bktextile@inpl.com" },
+      where: { email: "meditec@inpl.com" },
     });
 
     if (!adminExists) {
       const bcrypt = await import("bcryptjs");
-      const hashedPassword = await bcrypt.hash("r_bktextilepasJK76^h", 10);
+      const hashedPassword = await bcrypt.hash("r_meditecpasJK76^h", 10);
 
       await AdminUser.create({
-        email: "bktextile@inpl.com",
+        email: "meditec@inpl.com",
         password: hashedPassword,
         is_verify: true,
         role: "admin",
