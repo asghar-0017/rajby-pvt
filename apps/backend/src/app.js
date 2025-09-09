174b8e82f4307f123ad3e4b7734874c4c179824b
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-09 13:17:49 -0700

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://syahimpex.inplsoftwares.online",
+          "https://china-youncheng.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://syahimpex.inplsoftwares.online",
-      "https://syahimpex.inplsoftwares.online",
+      "https://china-youncheng.inplsoftwares.online",
+      "https://china-youncheng.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
