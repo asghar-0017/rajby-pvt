9646aa683a8ce7ccd140b3eb4f1041e206afffe0
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-09 13:40:33 -0700

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://mayanipoly.inplsoftwares.online",
+          "https://spoltlit.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://mayanipoly.inplsoftwares.online",
-      "https://mayanipoly.inplsoftwares.online",
+      "https://spoltlit.inplsoftwares.online",
+      "https://spoltlit.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
