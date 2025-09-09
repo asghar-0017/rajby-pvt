ccf8703f4dae77a6436c1579623dfbff16a68f22
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-09 13:57:04 -0700

    .

--- apps/frontend/src/component/BuyerModal.jsx
+++ apps/frontend/src/component/BuyerModal.jsx
@@ -331,7 +331,7 @@ const BuyerModal = ({ isOpen, onClose, onSave, buyer }) => {
       setBuyerRegistrationHint("");
 
       const response = await fetch(
-        "https://einv-aaafm.inplsoftwares.online/api/buyer-check",
+        "https://premierpipe.inplsoftwares.online/api/buyer-check",
         {
           method: "POST",
           headers: {
