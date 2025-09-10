4a8b51a9b0cb36ad5407cb8588a93e220fc7ab66
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-10 14:19:24 -0700

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://signs-now.inplsoftwares.online",
+          "http://threadzpoly.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://signs-now.inplsoftwares.online",
-      "https://signs-now.inplsoftwares.online",
+      "http://threadzpoly.inplsoftwares.online",
+      "http://threadzpoly.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
