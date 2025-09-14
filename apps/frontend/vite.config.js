2030ce3d2b17e0d9993567806de15609c2279b60
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-14 15:57:02 -0700

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://multimed.inplsoftwares.online",
+        target: "https://sardarhos.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
