7b52e0f51e458cd1d068b848b05b3898500462b5
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-16 00:46:02 +0500

    .

--- apps/frontend/src/component/InvoiceUploader.jsx
+++ apps/frontend/src/component/InvoiceUploader.jsx
@@ -923,7 +923,7 @@ const InvoiceUploader = ({ onUpload, onClose, isOpen, selectedTenant }) => {
       }
       try {
         const resp = await fetch(
-          "https://mayanipoly.inplsoftwares.online/api/buyer-check",
+          "https://hubpoly-packages.inplsoftwares.online/api/buyer-check",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
