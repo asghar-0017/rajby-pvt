c69a1e490a205a66fadb4de437f523c59be5f111
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-16 01:51:42 +0500

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://unitedtrimming.inplsoftwares.online",
+          "https://tsbuttons.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://unitedtrimming.inplsoftwares.online",
-      "https://unitedtrimming.inplsoftwares.online",
+      "https://tsbuttons.inplsoftwares.online",
+      "https://tsbuttons.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
