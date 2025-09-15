fc238893f4dbb365bff7612bc3ef9c6b5346e9ba
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-16 00:33:04 +0500

    .

--- test-bulk-upload.js
+++ test-bulk-upload.js
@@ -52,7 +52,7 @@ const testBulkUpload = async () => {
     });
 
     const response = await fetch(
-      "https://premierpipe.inplsoftwares.online/api/tenant/tenant_1756409312403_uo1hmt4tz/invoices/bulk",
+      "https://einv-aaafm.inplsoftwares.online/api/tenant/tenant_1756409312403_uo1hmt4tz/invoices/bulk",
       {
         method: "POST",
         headers: {
