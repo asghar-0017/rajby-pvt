6aa03aa5f19ab54325c4eeb780fd576eb393884a
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-16 01:00:39 +0500

    .

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -17,7 +17,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       "/api": {
-        target: "https://united-tubes.inplsoftwares.online",
+        target: "https://superstar.inplsoftwares.online",
         changeOrigin: true,
         secure: false,
       },
