557420d7c8fc44abfa66affc9c14894e5df80de9
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-22 20:58:14 +0500

    .

--- apps/backend/src/dbConnector/mysqlConnector.js
+++ apps/backend/src/dbConnector/mysqlConnector.js
@@ -34,15 +34,15 @@ const mysqlConnector = async (dbConfig, logger) => {
 const initializeAdminUser = async () => {
   try {
     const adminExists = await AdminUser.findOne({
-      where: { email: "mapakedible1@inpl.com" },
+      where: { email: "diamondindustries@inpl.com" },
     });
 
     if (!adminExists) {
       const bcrypt = await import("bcryptjs");
-      const hashedPassword = await bcrypt.hash("r_mapakedible1pasJK76^h", 10);
+      const hashedPassword = await bcrypt.hash("r_diamondindustriespasJK76^h", 10);
 
       await AdminUser.create({
-        email: "maltextexport1@inpl.com",
+        email: "diamondindustries@inpl.com",
         password: hashedPassword,
         is_verify: true,
         role: "admin",
