9362e648e9d19df18aed08ae347334df57112959
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-23 14:21:58 +0500

    .

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -48,7 +48,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://diamondindustries.inplsoftwares.online",
+          "https://sardarhos.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -62,8 +62,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://diamondindustries.inplsoftwares.online",
-      "https://diamondindustries.inplsoftwares.online",
+      "https://sardarhos.inplsoftwares.online",
+      "https://sardarhos.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
