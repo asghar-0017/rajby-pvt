d9896ed838bb09dcdafa4f8bbb6f61d1475f44fe
Asghar Ali <asghar.usman0017@gmail.com>
2025-09-29 15:31:26 +0500

    /

--- apps/backend/src/app.js
+++ apps/backend/src/app.js
@@ -55,7 +55,7 @@ app.use(
         connectSrc: [
           "'self'",
           "https://gw.fbr.gov.pk",
-          "https://fpmodarba.inplsoftwares.online",
+          "https://dystar.inplsoftwares.online",
         ],
         scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'"],
         styleSrc: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com"],
@@ -69,8 +69,8 @@ app.use(
   cors({
     origin: [
       "http://localhost:5174",
-      "https://fpmodarba.inplsoftwares.online",
-      "https://fpmodarba.inplsoftwares.online",
+      "https://dystar.inplsoftwares.online",
+      "https://dystar.inplsoftwares.online",
       "https://fbrtestcase.inplsoftwares.online",
       "*",
     ],
